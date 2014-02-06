043bcc5aed83f0dced627a39092e46c0ca95e204
telv1n <keith.smith@outlook.com>
2014-02-05 23:15:56 -0700

    simple_shell.asm wording
    adjusted comments to makes sure that they are visible and look ok

--- basic_vuln/simple_shell.asm
+++ basic_vuln/simple_shell.asm
@@ -1,4 +1,6 @@
-; simple_shell.asm is an assembly file I created which contains basic shellcode. It makes a call to execve that executes /bin/sh, spawning a terminal.
+; simple_shell.asm is an assembly file I created which contains basic shellcode. 
+; It makes a call to execve that executes /bin/sh, spawning a terminal
+
 ; AUTHOR: Keith S.
 
 section .data
